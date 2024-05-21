1e341277ceb41bf44c1b681571c86a4c406d30a8
nicolasriverocorvalan <riveronicolas@gmail.com>
2024-05-20 23:24:02 -0300

    ethernaut

--- ethernaut/3-CoinFlip/script/DeployCoinFlipAttack.s.sol
+++ ethernaut/3-CoinFlip/script/DeployCoinFlipAttack.s.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
 import {CoinFlipAttack} from "../src/CoinFlipAttack.sol";
